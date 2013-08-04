a37ddab45fcecb69ac66607a5b8852d5c0d3b537
Pascal Cohen <pascohen@gmail.com>
2013-08-04 21:44:17 +0200

    improved response management

--- src/main/scala/dslprez/continuations/Ask.scala
+++ src/main/scala/dslprez/continuations/Ask.scala
@@ -7,22 +7,16 @@ import scala.util.continuations._
 object Ask {
 
    // The continuation
-   var cont: Unit => Unit = _
-
-   // last answer obtained - used as return value
-   // of the conitnuation
-   var lastAnswer: String = _
+   var cont: Option[String => Unit] = None
 
    def ask(query:String):String @cps[Unit] = {
       shift { 
-         k: (Unit => Unit) => 
+         k: (String => Unit) => 
             // Make the continuation available for call
-            cont = k 
+            cont = Some(k) 
             // Display the query and then quit the continuation
             println(query)
          } 
-         // Continuation restarts and returns the last answer
-         lastAnswer
    }
 
    // Starts the DSL - basically a set of DSL rules
@@ -34,5 +28,8 @@ object Ask {
    }
 
    // get back into the DSL flow by calling the stored continuation
-   def answer(answer:String) = {lastAnswer= answer; cont() }
+   def answer(answer:String) = {
+	if (!cont.isEmpty) cont.get(answer) 
+        cont=None
+   }
 }
