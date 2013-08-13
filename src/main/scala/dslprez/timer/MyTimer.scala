93abac1f1c0ac8c02133d58d93d2821619e44ec9
Pascal Cohen <pascohen@gmail.com>
2013-08-13 21:35:19 +0200

    replaced lazy mechanism with a temporar mechanism

--- src/main/scala/dslprez/timer/MyTimer.scala
+++ src/main/scala/dslprez/timer/MyTimer.scala
@@ -1,5 +1,13 @@
 package dslprez.timer
 
 object MyTimer {
-  lazy val getStartTime = System.currentTimeMillis()
+  
+  var startTime:Long = 0
+  
+  def reinit = startTime = 0
+  
+  def getStartTime = { 
+    if (startTime == 0) startTime = System.currentTimeMillis()
+    startTime
+  }
 }
