c1c96b117fc9afa81f4a15519be9ad34d6a5c7f3
Pascal Cohen <pascohen@gmail.com>
2013-08-25 21:08:27 +0200

    add implicit conversion to direction

---
+++ src/main/scala/dslprez/scala/game/InvalidDirection.scala
@@ -0,0 +1,3 @@
+package dslprez.scala.game
+
+class InvalidDirection extends Exception
--- src/main/scala/dslprez/scala/game/game.scala
+++ src/main/scala/dslprez/scala/game/game.scala
@@ -3,6 +3,14 @@ import scala.language.implicitConversions
 
 package dslprez.scala {
   package object game {
+  
+    implicit def toDirection(s:String) = s match {
+      case "up" => dslprez.scala.game.up
+      case "down" => dslprez.scala.game.down
+      case "left" => dslprez.scala.game.left
+      case "right" => dslprez.scala.game.right
+      case _ => throw new InvalidDirection()
+    }
    
     def toSet(a: Array[Array[Int]]) = (for (i<-0 until a.length;j<-0 until a(i).length if (a(i)(j) == 1)) yield (i,j)).toSet
     
